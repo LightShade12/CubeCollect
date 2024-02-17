2c5099809e61f3b2380ad61925f611c45d55a2ce
LightShade12 <subhamswostikpradhan2004@gmail.com>
2024-02-17 21:27:02 +0530

    init

---
+++ source/entity/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const mouse_sens:float=0.2;
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var head = $head
+
+func _ready():
+	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED;
+	pass
+
+func _input(event):
+	if (event is InputEventMouseMotion):
+		rotate_y(deg_to_rad(-event.relative.x*mouse_sens))
+		
+		head.rotate_x(deg_to_rad(-event.relative.y*mouse_sens))
+
+		head.rotation.x=clamp(head.rotation.x,deg_to_rad(-90),deg_to_rad(90));
+
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("key_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_front", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
